449e9dc85e5bbecce341dd9de0e23b599245f1d1
k-shreehari7 <hari18031911@gmail.com>
2022-03-29 22:19:27 +0530

    After Adding dark mode for body and navigator

--- src/App.js
+++ src/App.js
@@ -1,15 +1,40 @@
 
+import { useState } from 'react';
 import './App.css';
 import './components/Navbar';
 import Navbar from './components/Navbar';
 import Textform from './components/Textform';
+// import About from './components/About';
+
 
 function App() {
+  const [mode,setMode]=useState("light");
+  const [tmode,setTmode]=useState("dark");
+
+
   return (
     <>
-        <Navbar title="Textutisl" aboutText="About us"/>
+       
+        <Navbar title="Textutisl" aboutText="About us" mode={mode} toggleMode={()=>{
+
+          if(mode==="light")
+          {
+            setMode("dark");
+            setTmode("light");
+            document.body.style.backgroundColor="grey"
+          }
+          else
+          {
+            setMode("light");
+            setTmode("dark");
+            document.body.style.backgroundColor="white"
+          
+          }
+        }} tmode={tmode}/>
         <div className="container my-3 ">
-          <Textform heading='Enter the text to analyse'/>
+          <Textform heading='Enter the text to analyse' tmode={tmode}/>
+
+          {/* <About/> */}
 
         </div>
         
--- src/components/Textform.js
+++ src/components/Textform.js
@@ -3,6 +3,8 @@ import React,{useState} from 'react'
 
 export default function Textform(props) {
 
+    let newtet='';
+
     const handleUpClick=()=>{
         console.log('BUTTON WAS PRESSED');
        
@@ -19,25 +21,61 @@ export default function Textform(props) {
         console.log('ON CHANGE');
         setText(event.target.value);
     }
+
+    const handleCpClick=()=>{
+        var text=document.getElementById('exampleFormControlTextarea1');
+        text.select();
+        navigator.clipboard.writeText(text.value);
+    }
+
+
+    const handleEmClick=()=>{
+        let arr=text.split(" ")
+        arr.forEach((txt,index)=>{
+            if(txt.indexOf('@')===-1)
+            {
+                
+            }
+            else
+            {
+                newtet=txt+'\n'+txt;
+                setText(newtet);
+            }
+
+
+        });
+
+    }
     const [text,setText]=useState('Enter the text here');
   return (
 <div>
     <div className="container">
-       <h1>{props.heading}</h1>
+       <h1 className={`text-${props.tmode}`}>{props.heading}</h1>
        <div className="mb-3">
-       <textarea className="form-control" value={text} onChange={handleOnChange} id="exampleFormControlTextarea1" rows="3"></textarea>
+       <textarea className="form-control" value={text} onChange={handleOnChange} id="exampleFormControlTextarea1" rows="3" style={{
+
+           backgroundColor: props.tmode==='dark'?'white':'grey'
+       }}></textarea>
        </div>
-       <button className="btn btn-primary" onClick={handleUpClick}>Convert to uppercase</button>
+       <div className="conatiner my-4">
+                  <button className="btn btn-primary" onClick={handleUpClick}>Convert to uppercase</button>
        <button className="btn btn-primary mx-4" onClick={handleLoClick}>Convert to lowercase</button>
+        <button className="btn btn-primary mx-4" onClick={handleEmClick}>Extract Email</button>
+        <button className="btn btn-primary mx-4" onClick={handleCpClick}>Copy Text</button>
+
+
+
+       </div>
+
         
     </div>
 
-    <div className="container my-2">
-        <h2>Your Text Summary</h2>
-        <p>{text.split(" ").length} words,{text.length}chaaracters</p>
-        <p>{text.split(" ").length * 0.08} Minutes to read</p>
-        <h2>Preview</h2>
-        <p>{text.toLocaleLowerCase()}</p>
+    <div className="container my-5">
+        <h2 className={`text-${props.tmode}`}>Your Text Summary</h2>
+        <p className={`text-${props.tmode}`}>{text.split(" ").length} words,{text.length}chaaracters</p>
+        <p className={`text-${props.tmode}`}>{text.split(" ").length * 0.08} Minutes to read</p>
+        <h2 className={`text-${props.tmode}`}>Preview</h2>
+        <p className={`text-${props.tmode}`}>{(text.length>0)?text.toLocaleLowerCase():'Enter the text to preview it'}</p>
 
     </div>
 
